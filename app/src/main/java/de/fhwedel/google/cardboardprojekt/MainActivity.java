dd30afaf586a14c06b6ea966fee269af14b2586e
Mervyn McCreight <mervyn.mccreight@gmail.com>
2015-03-31 13:55:03 +0200

    getting started :)

--- app/src/main/java/de/fhwedel/google/cardboardprojekt/MainActivity.java
+++ app/src/main/java/de/fhwedel/google/cardboardprojekt/MainActivity.java
@@ -3,13 +3,49 @@ package de.fhwedel.google.cardboardprojekt;
 import android.os.Bundle;
 
 import com.google.vrtoolkit.cardboard.CardboardActivity;
+import com.google.vrtoolkit.cardboard.CardboardView;
+import com.google.vrtoolkit.cardboard.Eye;
+import com.google.vrtoolkit.cardboard.HeadTransform;
+import com.google.vrtoolkit.cardboard.Viewport;
+
+import javax.microedition.khronos.egl.EGLConfig;
 
 
-public class MainActivity extends CardboardActivity {
+public class MainActivity extends CardboardActivity implements CardboardView.StereoRenderer {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onNewFrame(HeadTransform headTransform) {
+
+    }
+
+    @Override
+    public void onDrawEye(Eye eye) {
+
+    }
+
+    @Override
+    public void onFinishFrame(Viewport viewport) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(int width, int height) {
+
+    }
+
+    @Override
+    public void onSurfaceCreated(EGLConfig eglConfig) {
+
+    }
+
+    @Override
+    public void onRendererShutdown() {
+
     }
 }
